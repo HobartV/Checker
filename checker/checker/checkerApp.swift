e7728756adf20e8c93550b0641801f83f57174c0
HobartV <102161641+HobartV@users.noreply.github.com>
2022-04-27 00:30:00 +0800

    Add files via upload

---
+++ checker/checker/checkerApp.swift
@@ -0,0 +1,17 @@
+//
+//  checkerApp.swift
+//  checker
+//
+//  Created by User11 on 2022/4/26.
+//
+
+import SwiftUI
+
+@main
+struct checkerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
